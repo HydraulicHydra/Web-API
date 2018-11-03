d9b0f7ed989cd87d1b86041b98ee90e08b151250
Garrett Surber <38016069+HydraulicHydra@users.noreply.github.com>
2018-11-03 19:47:07 -0400

    Add files via upload

---
+++ TodoApi/TodoApi/Controllers/ToDoItemsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApi.Interfaces;
+using ToDoApi.Models;
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoItemsController : Controller
+    {
+        private readonly IToDoRepository _toDoRepository;
+
+        public ToDoItemsController(IToDoRepository toDoRepository)
+        {
+            _toDoRepository = toDoRepository;
+        }
+    }
+}
